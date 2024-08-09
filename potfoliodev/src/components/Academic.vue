0fbc0e4fd5b6238fc37d53a4ad3749e240552bcd
Emanuel Lima <emanuelrfplima@hotmail.com>
2024-08-09 10:35:48 -0300

    other fix

--- potfoliodev/src/components/Academic.vue
+++ potfoliodev/src/components/Academic.vue
@@ -21,7 +21,7 @@ export default {
 <div>
     <div v-if="Store.leanguage === 'PT-BR'" class="mb-5 d-flex justify-content-center">
         <div style="margin-top: 7rem !important;">
-            <div class="d-flex justify-content-center">
+            <div class="d-flex justify-content-center FontProjectMedium">
                 <span><strong><h2 style="color: rgb(40, 40, 40);">QUALIFICAÇÃO</h2></strong></span>
             </div>
             <div class="d-flex justify-content-center">
@@ -35,7 +35,7 @@ export default {
             </div>
 
             <div v-if="academic">
-                <div class="d-flex justify-content-center text-center row" style="padding-top:2.5rem;column-gap:2rem;">
+                <div class="d-flex justify-content-center text-center row FontProjectMedium" style="padding-top:2.5rem;column-gap:2rem;">
                     <div class="col-sm mb-2">
                         <h3 style="color:#d01bdb;"><b>UDEMY</b></h3>
                         <h6 style="color:#d01bdb;"><b>DESENVOLVIMENTO WEB FULLSTACK</b></h6>
@@ -47,7 +47,7 @@ export default {
                         <span style="color:#2856d7;text-align:center;"><strong>2017</strong></span>
                     </div>
                 </div>
-                <div class="d-flex justify-content-center text-center row" style="column-gap:2rem;">
+                <div class="d-flex justify-content-center text-center row FontProjectMedium" style="column-gap:2rem;">
                     <div class="col-sm mb-2">
                         <h3 style="color:#0c6427;"><b>IFRN</b></h3>
                         <h6 style="color:#0c6427;"><b>MÉDIO/TÉCNICO EM INFORMÁTICA PARA INTERNET</b></h6>
@@ -62,7 +62,7 @@ export default {
                 </div>
             </div>
             <div v-if="professional">
-                <div class="d-flex justify-content-center text-center row" style="padding-top:2.5rem;column-gap:2rem;">
+                <div class="d-flex justify-content-center text-center row FontProjectMedium" style="padding-top:2.5rem;column-gap:2rem;">
                     <div class="col-sm mb-2">
                         <h3 style="color:#ff0b0b;"><b>CNPQ</b></h3>
                         <h6 style="color:#ff0b0b;"><b>INICIAÇÃO A PESQUISA ESCOLAR</b></h6>
@@ -74,7 +74,7 @@ export default {
                         <span style="color:#fb0fff;text-align:center;"><strong>2021 - 2022</strong></span>
                     </div>
                 </div>
-                <div class="d-flex justify-content-center text-center row" style="column-gap:2rem;">
+                <div class="d-flex justify-content-center text-center row FontProjectMedium" style="column-gap:2rem;">
                     <div class="col-sm mb-2">
                         <h3 style="color:#0c6427;"><b>AMBIENTAL L/S LTDA</b></h3>
                         <h6 style="color:#0c6427;"><b>DESENVOLVEDOR WEB FRONT-END</b></h6>
@@ -92,7 +92,7 @@ export default {
 
     <div v-if="Store.leanguage === 'EN-US'" class="mb-5 d-flex justify-content-center">
         <div style="margin-top: 7rem !important;">
-            <div class="d-flex justify-content-center">
+            <div class="d-flex justify-content-center FontProjectMedium">
                 <span><strong><h2 style="color: rgb(40, 40, 40);">QUALIFICATION</h2></strong></span>
             </div>
             <div class="d-flex justify-content-center">
@@ -106,7 +106,7 @@ export default {
             </div>
 
             <div v-if="academic">
-                <div class="d-flex justify-content-center text-center row" style="padding-top:2.5rem;column-gap:2rem;">
+                <div class="d-flex justify-content-center text-center row FontProjectMedium" style="padding-top:2.5rem;column-gap:2rem;">
                     <div class="col-sm mb-2">
                         <h3 style="color:#d01bdb;"><b>UDEMY</b></h3>
                         <h6 style="color:#d01bdb;"><b>FULLSTACK WEB DEVELOPMENT</b></h6>
@@ -118,7 +118,7 @@ export default {
                         <span style="color:#2856d7;text-align:center;"><strong>2017</strong></span>
                     </div>
                 </div>
-                <div class="d-flex justify-content-center text-center row" style="column-gap:2rem;">
+                <div class="d-flex justify-content-center text-center row FontProjectMedium" style="column-gap:2rem;">
                     <div class="col-sm mb-2">
                         <h3 style="color:#0c6427;"><b>IFRN</b></h3>
                         <h6 style="color:#0c6427;"><b>HIGH SCHOOL/TECHNICAL IN COMPUTING FOR INTERNET</b></h6>
@@ -133,7 +133,7 @@ export default {
                 </div>
             </div>
             <div v-if="professional">
-                <div class="d-flex justify-content-center text-center row" style="padding-top:2.5rem;column-gap:2rem;">
+                <div class="d-flex justify-content-center text-center row FontProjectMedium" style="padding-top:2.5rem;column-gap:2rem;">
                     <div class="col-sm mb-2">
                         <h3 style="color:#ff0b0b;"><b>CNPQ</b></h3>
                         <h6 style="color:#ff0b0b;"><b>SCIENTIFIC INITIATION SCHOLARSHIP</b></h6>
@@ -145,7 +145,7 @@ export default {
                         <span style="color:#fb0fff;text-align:center;"><strong>2021 - 2022</strong></span>
                     </div>
                 </div>
-                <div class="d-flex justify-content-center text-center row" style="column-gap:2rem;">
+                <div class="d-flex justify-content-center text-center row FontProjectMedium" style="column-gap:2rem;">
                     <div class="col-sm mb-2">
                         <h3 style="color:#0c6427;"><b>AMBIENTAL L/S LTDA</b></h3>
                         <h6 style="color:#0c6427;"><b>FRONT-END WEB DEVELOPER</b></h6>
